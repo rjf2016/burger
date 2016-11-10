1dc6330f7eb9f071ca270ec765b71efcec77892e
Rick Fahey <rickyfahey@hotmail.com>
2016-11-10 12:34:36 -0500

    uploaded all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name) values ("Chicken Bacon Burger");
+INSERT INTO burgers (burger_name) values ("All American Burger");
+INSERT INTO burgers (burger_name) values ("Krabby Patty");
